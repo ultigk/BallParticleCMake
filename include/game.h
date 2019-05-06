49dbf97dbd499d36276cc9fd7f647fe133caad37
ultigk <ultigk@mail.ru>
2019-05-06 19:47:39 +0300

    Add files via upload

---
+++ include/game.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "window.h"
+#include "ParticleSystem.h"
+#include <SFML/Graphics.hpp>
+
+//Particle -> ParticleSystem
+
+class Game
+{
+public:
+    Game();
+    virtual ~Game() = default;
+
+    void Run();
+    void HandleInput();
+    void Update(const float dt);
+    void Render();
+
+
+private:
+    Window main_window_;
+
+    ParticleSystem particles_;
+};
